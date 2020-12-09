6ca58430ffdc7608ea354c7cb878f7ba27b0ad55
joco <13711619148@163.com>
2020-12-09 09:59:10 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ var path = require('path');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   entry: './src/index.js',
   output: {
    
