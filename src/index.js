acccd3f6f1936b41189341baad8aebe558ade5bf
joco <13711619148@163.com>
2020-12-09 14:28:57 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import x from './x.js'
 import png from './assets/1.png'
 
-console.log(png)
+// console.log(png)
+
+console.log('你好啊')
 
 
-// const div=document.getElementById('app')
+const div=document.getElementById('app')
 
 // div.innerHTML=`<img src="${png}">`
 
