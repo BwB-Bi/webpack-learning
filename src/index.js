9b6327dba5c3988e3cd4cf3a02bb3ca1ec3c9537
joco <13711619148@163.com>
2020-12-09 13:43:22 +0800

    准备上传了

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-import x from './assets/x.js';
-console.log(x)
+import x from './x.js'
+import png from './assets/1.png'
+
+console.log(png)
+
+const div=document.getElementById('app')
+
+div.innerHTML=`<img src="${png}">`
+
+const button = document.createElement('button')
+button.innerHTML='懒加载'
+button.onclick=()=>{
+    const promise=import('./lazy')
+    promise.then((module)=>{
+        const fn=module.default
+        fn()
+    }, ()=>{
+
+    })
+}
+
+div.appendChild(button)
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,7 +12,39 @@ module.exports = {
     title: '宝文-写代码',
     template: 'src/assets/index.html'
   })
-]
+],
+module: {
+    rules: [
+    {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            use:["file-loader"]
+       },
+    {
+            test: /\.styl$/,
+
+            use: ['style-loader','css-loader','stylus-loader'] 
+         },
+     {
+            test: /\.less$/,
+
+            use: ['style-loader','css-loader','less-loader'] 
+         },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          
+          "css-loader",
+         {
+             loader:"sass-loader",
+             options:{
+                 implementation: require('dart-sass')
+             }
+         }
+        ],
+      },
+    ],
+  }
 };
 
 
